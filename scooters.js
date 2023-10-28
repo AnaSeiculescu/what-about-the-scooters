6b0229f16dbffeb913dfcea8afe7d35d8d720244
AnaSeiculescu <anaseiculescu@gmail.com>
2023-10-28 13:02:43 +0300

    user page has a map to search and info about scooters

---
+++ scooters.js
@@ -0,0 +1,38 @@
+const search_map = document.getElementById('map');
+
+/* PE HARTA se pot identifica STATIILE de trotinete, trotinete disponibile in statie, dar si 
+TROTINETE care sunt pozitionate/lasate IN AFARA STATIILOR 
+    DECI - pe harta se vor gasi trotinetele disponibile de selectat */
+
+function generate_map(map_el = search_map, lat_val = 45.760696, lng_val = 21.226788, descr_val = 'TImisoara') {
+    
+    let map_options_obj = {
+        zoom: 12,
+        center: {lat: lat_val, lng: lng_val},
+    }
+
+    let map_obj = new google.maps.Map(map_el, map_options_obj);
+
+    let marker_options_obj = {
+        position: map_options_obj.center,
+        map: map_obj,
+    };
+
+    let marker_obj = new google.maps.Marker(marker_options_obj);
+
+    let info_options_obj = {
+
+        content: '<h4>' + descr_val + '</h4>',
+
+    };
+
+    let info_obj = new google.maps.InfoWindow(info_options_obj);
+    info_obj.open(map_obj, marker_obj);
+
+    marker_obj.addListener("click", function(ev) {
+        info_obj.open(map_obj, marker_obj);
+    });
+
+}
+
+generate_map();
